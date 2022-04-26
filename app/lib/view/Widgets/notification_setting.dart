2d384c7e9ddc45ae35d77ecaa4aa4e7c5d7c0bd0
up201907284 <up201907284@fe.up.pt>
2022-04-26 12:00:31 +0100

    updated notification setting's fornt-end

--- app/lib/view/Widgets/notification_setting.dart
+++ app/lib/view/Widgets/notification_setting.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NotificationSetting extends StatefulWidget {
@@ -5,15 +6,17 @@ class NotificationSetting extends StatefulWidget {
   bool _switched;
   Function(bool) _onChanged;
 
-
-  NotificationSetting(String notificationName, {bool switched, Function(bool) onChanged}) {
+  NotificationSetting(String notificationName,
+      {bool switched, Function(bool) onChanged}) {
     this._notificationName = notificationName;
     this._switched = switched;
     this._onChanged = onChanged;
   }
 
   @override
-  State<NotificationSetting> createState() => _NotificationSettingsState(_notificationName, switched: _switched, onChanged: _onChanged);
+  State<NotificationSetting> createState() =>
+      _NotificationSettingsState(_notificationName,
+          switched: _switched, onChanged: _onChanged);
 }
 
 class _NotificationSettingsState extends State<NotificationSetting> {
@@ -22,7 +25,8 @@ class _NotificationSettingsState extends State<NotificationSetting> {
   Function(bool) _onChanged;
   double _timerSliderValue = 0;
 
-  _NotificationSettingsState(String notificationName, {bool switched, Function(bool) onChanged}) {
+  _NotificationSettingsState(String notificationName,
+      {bool switched, Function(bool) onChanged}) {
     this._notificationName = notificationName;
     this._switched = switched;
     this._onChanged = onChanged;
@@ -47,23 +51,34 @@ class _NotificationSettingsState extends State<NotificationSetting> {
       columnChildren.add(
         Slider(
           value: _timerSliderValue,
+          min: 0,
           max: 100,
-          divisions: 5,
-          label: _timerSliderValue.round().toString(),
+          divisions: 10,
+          //label: _timerSliderValue.round().toString(),
           onChanged: (double value) {
             setState(() {
               _timerSliderValue = value;
             });
           },
-        )
+          activeColor: Colors.brown[300],
+          inactiveColor: Colors.deepOrange[400],
+        ),
       );
+      columnChildren.add(
+        SizedBox(height: 30),
+      );
+      if (_notificationName == 'Início de Aulas') {
+        columnChildren
+            .add(Text("$_timerSliderValue minutos antes da próxima aula."));
+      } else {
+        columnChildren.add(Text(
+            "$_timerSliderValue dias antes do próximo pagamento de serviços."));
+      }
     }
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        children: columnChildren
-      ),
+      child: Column(children: columnChildren),
     );
   }
 }
