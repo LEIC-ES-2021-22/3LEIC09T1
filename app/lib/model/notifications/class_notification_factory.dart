076012ce829413ddb4dc90995ea544c6afe0419d
Francisco Oliveira <up201907361@fe.up.pt>
2022-05-23 15:55:38 +0100

    [Change] Moved calculateTime to another file because it can be useful to schedule the notification scheduling work

--- app/lib/model/notifications/class_notification_factory.dart
+++ app/lib/model/notifications/class_notification_factory.dart
@@ -3,6 +3,7 @@ import 'package:uni/model/entities/lecture.dart';
 import 'package:uni/model/notifications/notification.dart';
 import 'package:uni/model/notifications/notification_factory.dart';
 import 'package:uni/utils/constants.dart';
+import 'package:uni/utils/time_zone_utils.dart' as tzu;
 import 'package:timezone/timezone.dart' as tz;
 
 class ClassNotificationFactory extends NotificationFactory<Lecture> {
@@ -21,14 +22,11 @@ class ClassNotificationFactory extends NotificationFactory<Lecture> {
   }
 
   tz.TZDateTime calculateTime(Lecture notificationModel, int antecedence) {
-    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
-    final int distance = ((notificationModel.day + 1) % 7 - now.weekday) % 7;
-    final tz.TZDateTime date =
-        tz.TZDateTime(tz.local, now.year, now.month, now.day)
-            .add(Duration(days: distance))
-            .add(Duration(hours: notificationModel.startTimeHours))
-            .add(Duration(
-                minutes: notificationModel.startTimeMinutes - antecedence));
-    return date;
+    return tzu.calculateTime(
+        indexDayOfWeek: notificationModel.day + 1,
+        antecedence: antecedence,
+        startTimeHours: notificationModel.startTimeHours,
+        startTimeMinutes: notificationModel.startTimeMinutes
+    );
   }
 }
---
+++ app/lib/utils/time_zone_utils.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+/// Creates a TZDateTime for the same week day in the next week
+/// The week starts on monday
+tz.TZDateTime calculateTime({
+  @required int indexDayOfWeek,
+  int antecedence = 0,
+  int startTimeHours = 1,
+  int startTimeMinutes = 0}
+  ) {
+  final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+  final int distance = (indexDayOfWeek % 7 - now.weekday) % 7;
+  final tz.TZDateTime date =
+  tz.TZDateTime(tz.local, now.year, now.month, now.day)
+      .add(Duration(days: distance))
+      .add(Duration(hours: startTimeHours))
+      .add(Duration(
+      minutes: startTimeMinutes - antecedence));
+  return date;
+}
