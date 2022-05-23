808f192e8b9a281ef30acd62f36d02e83be91dd4
Francisco Oliveira <up201907361@fe.up.pt>
2022-05-23 18:42:25 +0100

    Created all the tests to calculateDayInNextWeek

--- app/lib/utils/time_zone_utils.dart
+++ app/lib/utils/time_zone_utils.dart
@@ -1,21 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 /// Creates a TZDateTime for the same week day in the next week
-/// The week starts on monday
+/// The week starts on monday which has a value of 0
 tz.TZDateTime calculateDayInNextWeek({
   @required tz.TZDateTime now,
   @required int indexDayOfWeek,
-  int antecedence = 0,
+  Duration antecedence = Duration.zero,
   int startTimeHours = 1,
   int startTimeMinutes = 0}
   ) {
-  final int distance = (indexDayOfWeek % 7 - now.weekday) % 7;
+  final nowWeekDayStartingZero = now.weekday - 1;
+  final int distance = nowWeekDayStartingZero > indexDayOfWeek ?
+    (indexDayOfWeek - nowWeekDayStartingZero) % 7:
+    7 + indexDayOfWeek - nowWeekDayStartingZero;
   final tz.TZDateTime date =
   tz.TZDateTime(tz.local, now.year, now.month, now.day)
       .add(Duration(days: distance))
       .add(Duration(hours: startTimeHours))
-      .add(Duration(
-      minutes: startTimeMinutes - antecedence));
+      .add(Duration(minutes: startTimeMinutes))
+      .subtract(antecedence);
   return date;
 }
---
+++ app/test/unit/notification/next_notification_date.dart
@@ -0,0 +1,81 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:uni/utils/time_zone_utils.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
+
+void main() {
+  tz.initializeTimeZones();
+  group('calculateDayInNextWeek tests', () {
+    test('Less than a week before next date 1', () {
+      final tz.TZDateTime now = tz.TZDateTime.local(2022, 05, 27, 12, 30, 13);
+      final tz.TZDateTime nextDay = calculateDayInNextWeek(
+          now: now,
+          indexDayOfWeek: 1,
+          startTimeHours: 14,
+          startTimeMinutes: 0
+      );
+      expect(nextDay.weekday, nextDay.weekday); //Expects the same day of week
+      expect(nextDay.day, 31);
+      expect(nextDay.hour, 14);
+      expect(nextDay.minute, 0);
+      expect(nextDay.year, 2022);
+    });
+    test('Less than a week before next date 2', () {
+      final tz.TZDateTime now = tz.TZDateTime.local(2022, 05, 27, 12, 30, 13);
+      final tz.TZDateTime nextDay = calculateDayInNextWeek(
+          now: now,
+          indexDayOfWeek: 0,
+          startTimeHours: 14,
+          startTimeMinutes: 0
+      );
+      expect(nextDay.weekday, nextDay.weekday); //Expects the same day of week
+      expect(nextDay.day, 30);
+      expect(nextDay.hour, 14);
+      expect(nextDay.minute, 0);
+      expect(nextDay.year, 2022);
+    });
+    test('More than a week before next date', () {
+      final tz.TZDateTime now = tz.TZDateTime.utc(2022, 05, 24, 12, 30, 13);
+      final tz.TZDateTime nextDay = calculateDayInNextWeek(
+          now: now,
+          indexDayOfWeek: 4,
+          startTimeHours: 14,
+          startTimeMinutes: 0
+      );
+      expect(nextDay.weekday, nextDay.weekday); //Expects the same day of week
+      expect(nextDay.day, 3);
+      expect(nextDay.hour, 14);
+      expect(nextDay.minute, 0);
+      expect(nextDay.year, 2022);
+    });
+    test('Next day is in a different year', () {
+      final tz.TZDateTime now = tz.TZDateTime.utc(2022, 12, 31, 12, 30, 13);
+      final tz.TZDateTime nextDay = calculateDayInNextWeek(
+          now: now,
+          indexDayOfWeek: 4,
+          startTimeHours: 14,
+          startTimeMinutes: 0
+      );
+      expect(nextDay.weekday, nextDay.weekday); //Expects the same day of week
+      expect(nextDay.day, 6);
+      expect(nextDay.hour, 14);
+      expect(nextDay.minute, 0);
+      expect(nextDay.year, 2023);
+    });
+    test('Antecedence changes hours', () {
+      final tz.TZDateTime now = tz.TZDateTime.local(2022, 05, 27, 12, 30, 13);
+      final tz.TZDateTime nextDay = calculateDayInNextWeek(
+          now: now,
+          indexDayOfWeek: 4,
+          startTimeHours: 9,
+          startTimeMinutes: 0,
+          antecedence: Duration(minutes: 15)
+      );
+      expect(nextDay.weekday, nextDay.weekday); //Expects the same day of week
+      expect(nextDay.day, 3);
+      expect(nextDay.hour, 8);
+      expect(nextDay.minute, 45);
+      expect(nextDay.year, 2022);
+    });
+  });
+}
