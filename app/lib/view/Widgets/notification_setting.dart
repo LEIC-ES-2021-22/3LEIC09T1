7071bfbd6bdf7bf4ce1bdc1a723e2811bc7bc1fd
Francisco Oliveira <up201907361@fe.up.pt>
2022-05-24 12:07:07 +0100

    [Rem] Reverted changes

--- app/lib/view/Widgets/notification_setting.dart
+++ app/lib/view/Widgets/notification_setting.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:uni/controller/local_storage/app_notification_preferences_database.dart';
-import 'package:uni/model/entities/notification_preference.dart';
 
 class NotificationSetting extends StatefulWidget {
   String _notificationName;
@@ -25,20 +23,13 @@ class _NotificationSettingsState extends State<NotificationSetting> {
   String _notificationName;
   bool _switched;
   Function(bool) _onChanged;
-  List<NotificationPreference> current = AppNotificationPreferencesDatabase()
-      .preferences() as List<NotificationPreference>;
-
-  double _timerSliderValue; // Class notification
+  double _timerSliderValue = 0;
 
   _NotificationSettingsState(String notificationName,
       {bool switched, Function(bool) onChanged}) {
     this._notificationName = notificationName;
     this._switched = switched;
     this._onChanged = onChanged;
-    this._timerSliderValue = current[current.indexWhere(
-            (element) => element.notificationType == _notificationName)]
-        .antecedence
-        .toDouble();
   }
 
   @override
@@ -83,28 +74,6 @@ class _NotificationSettingsState extends State<NotificationSetting> {
       } else {
         columnChildren.add(Text(
             "$_timerSliderValue dias antes do prazo do próximo pagamento."));
-      }
-
-      // Only consider active if the slider is set to a value greater than 0
-      bool _active = _timerSliderValue.round() > 0;
-      if (_active) {
-        //New preference
-        NotificationPreference _pref = NotificationPreference(
-            _active, _timerSliderValue.round(), _notificationName);
-
-        //Access database
-        AppNotificationPreferencesDatabase _db =
-            AppNotificationPreferencesDatabase();
-
-        //Update the preference list
-        Future<List<NotificationPreference>> _preflst = _db.preferences();
-        List<NotificationPreference> _preferences =
-            _preflst as List<NotificationPreference>;
-        //search and update the preference
-        _preferences[_preferences.indexWhere(
-                (element) => element.notificationType == _notificationName)] =
-            _pref;
-        _db.saveNewPreferences(_preferences);
       }
     }
 
