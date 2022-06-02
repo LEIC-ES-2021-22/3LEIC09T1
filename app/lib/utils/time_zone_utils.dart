74a9d4f9c0d65cdf064300bd1bed42d1f68fb973
marhcouto <marhcouto1101@gmail.com>
2022-06-02 09:05:51 +0100

    Refactored hours

--- app/lib/utils/time_zone_utils.dart
+++ app/lib/utils/time_zone_utils.dart
@@ -4,24 +4,25 @@ import 'package:timezone/timezone.dart' as tz;
 
 /// Creates a TZDateTime for the same week day in the next week
 /// The week starts on monday which has a value of 0
-tz.TZDateTime calculateNextDay({
-  @required tz.TZDateTime now,
-  @required int indexDayOfWeek,
-  Duration antecedence = Duration.zero,
-  int startTimeHours = 1,
-  int startTimeMinutes = 0,
-  int addedWeeks = 0}
-    ) {
-  final int distanceInDays = (indexDayOfWeek - (now.weekday - 1)) % 7;
-  tz.TZDateTime date =
-  tz.TZDateTime(tz.local, now.year, now.month, now.day)
+tz.TZDateTime calculateNextDay(
+    {@required tz.TZDateTime now,
+    @required int indexDayOfWeek,
+    Duration antecedence = Duration.zero,
+    int startTimeHours = 1,
+    int startTimeMinutes = 0,
+    int addedWeeks = 0}) {
+  final HOURS_IN_WEEK = 168;
+  final DAYS_IN_WEEK = 7;
+  final int distanceInDays =
+      (indexDayOfWeek - (now.weekday - 1)) % DAYS_IN_WEEK;
+  tz.TZDateTime date = tz.TZDateTime(tz.local, now.year, now.month, now.day)
       .add(Duration(days: distanceInDays))
       .add(Duration(hours: startTimeHours))
       .add(Duration(minutes: startTimeMinutes))
       .subtract(antecedence)
-      .add(Duration(hours: addedWeeks * 168));
+      .add(Duration(hours: addedWeeks * HOURS_IN_WEEK));
   if (date.isBefore(now)) {
-    date = date.add(Duration(hours: 168));
+    date = date.add(Duration(hours: HOURS_IN_WEEK));
   }
   return date;
 }
