1c1f5eacd6cffa3b0fe69815363e4eac05698c69
frpdoliv <fr.pdoliv@gmail.com>
2022-06-05 18:01:57 +0100

    Minor Changes

---
+++ app/test/unit/notification/always_fails.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:uni/model/entities/lecture.dart';
+
+void main() {
+  group('Test Test', () {
+    test('Always fails', (){
+      expect(1, 2);
+    });
+  });
+}
