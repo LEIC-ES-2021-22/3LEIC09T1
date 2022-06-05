b3133d392383da4e14357fe2ef5e418541e322d3
frpdoliv <fr.pdoliv@gmail.com>
2022-06-05 12:44:55 +0100

    Now notification page view is consistent with the database

--- app/lib/view/Pages/notification_settings_page_view.dart
+++ app/lib/view/Pages/notification_settings_page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:uni/controller/local_storage/app_notification_preferences_database.dart';
+import 'package:uni/model/entities/notification_preference.dart';
 import 'package:uni/utils/constants.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
 import 'package:uni/view/Widgets/notification_setting.dart';
@@ -10,6 +12,67 @@ class NotificationSettingsPageView extends StatefulWidget {
 }
 
 class NotificationSettingsPageViewState extends SecondaryPageViewState {
+  var _editedPreferences = false;
+  final Map<NotificationType, NotificationPreference> notificationSettings = {
+    NotificationType.classNotif: NotificationPreference(
+      antecedence: 0,
+      notificationType: NotificationType.classNotif.typeName,
+      isActive: false
+    )
+  };
+
+  NotificationSettingsPageViewState() {
+    retrieveSettingsFromDatabase();
+  }
+
+  retrieveSettingsFromDatabase() async {
+    final db = AppNotificationPreferencesDatabase();
+    for (var key in notificationSettings.keys) {
+      notificationSettings[key] = await db.getPreference(key);
+    }
+    if (mounted) {
+      setState(() => _editedPreferences = false);
+    }
+  }
+
+  getCommitButtons() {
+    if (_editedPreferences) {
+      return Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              child: Text('Descartar'),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey)
+              ),
+              onPressed: () {
+                retrieveSettingsFromDatabase();
+              },
+            ),
+            SizedBox(width: 15),
+            ElevatedButton(
+              child: Text('Guardar'),
+              onPressed: savePreferences,
+            ),
+          ]
+        )
+      );
+    }
+    return SizedBox.shrink();
+  }
+
+  savePreferences() async {
+    final db = AppNotificationPreferencesDatabase();
+    for (var value in notificationSettings.values) {
+      await db.replacePreference(value);
+    }
+    if (mounted) {
+      setState(() => _editedPreferences = false);
+    }
+  }
+
   @override
   Widget getBody(BuildContext context) {
     return Column(
@@ -19,12 +82,23 @@ class NotificationSettingsPageViewState extends SecondaryPageViewState {
         ),
         NotificationSetting(
           notificationType: NotificationType.classNotif,
-          switched: false,
+          switched: notificationSettings[NotificationType.classNotif].isActive,
           onSwitchChanged: (value) {
-
+            setState(() {
+              _editedPreferences = true;
+              notificationSettings[NotificationType.classNotif]
+                  .isActive = value;
+            });
           },
-          sliderValue: 10.0,
+          onSliderChanged: (value) {
+            setState(() => _editedPreferences = true);
+            notificationSettings[NotificationType.classNotif]
+                .antecedence = value.toInt();
+          },
+          initialSliderValue: notificationSettings[NotificationType.classNotif]
+              .antecedence,
         ),
+        getCommitButtons()
       ],
     );
   }
--- app/lib/view/Widgets/notification_setting.dart
+++ app/lib/view/Widgets/notification_setting.dart
@@ -2,19 +2,49 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:uni/utils/constants.dart';
 
-class NotificationSetting extends StatelessWidget {
+class NotificationSetting extends StatefulWidget {
   final NotificationType notificationType;
   final bool switched;
-  final double sliderValue;
+  final int initialSliderValue;
   final Function(bool) onSwitchChanged;
+  final Function(double) onSliderChanged;
 
   NotificationSetting({
     @required this.notificationType,
     @required this.switched,
     @required this.onSwitchChanged,
-    @required this.sliderValue
+    @required this.onSliderChanged,
+    @required this.initialSliderValue
   });
 
+  @override
+  State<NotificationSetting> createState() => _NotificationSettingState(
+      notificationType: this.notificationType,
+      switched: this.switched,
+      onSwitchChanged: this.onSwitchChanged,
+      onSliderChanged: this.onSliderChanged,
+      initialSliderValue: this.initialSliderValue
+  );
+}
+
+class _NotificationSettingState extends State<NotificationSetting> {
+  final NotificationType notificationType;
+  final bool switched;
+  final int initialSliderValue;
+  final Function(bool) onSwitchChanged;
+  final Function(double) onSliderChanged;
+  double sliderValue;
+
+  _NotificationSettingState({
+    @required this.notificationType,
+    @required this.switched,
+    @required this.onSwitchChanged,
+    @required this.onSliderChanged,
+    @required this.initialSliderValue
+  }) {
+    sliderValue = initialSliderValue.toDouble();
+  }
+  
   @override
   Widget build(BuildContext context) {
     List<Widget> columnChildren = [
@@ -39,6 +69,8 @@ class NotificationSetting extends StatelessWidget {
           divisions: notificationType.antecedenceGranularity,
           activeColor: Colors.brown[300],
           inactiveColor: Colors.deepOrange[400],
+          onChanged: (value) => setState(() => sliderValue = value),
+          onChangeEnd: onSliderChanged,
         ),
       );
       columnChildren.add(
