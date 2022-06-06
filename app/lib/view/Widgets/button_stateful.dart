d2950a3a1cad2f5003e48784878babbcf0f943d8
frpdoliv <fr.pdoliv@gmail.com>
2022-06-06 20:47:32 +0100

    Changed the way icons are switched

--- app/lib/view/Widgets/button_stateful.dart
+++ app/lib/view/Widgets/button_stateful.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
 
 class ScheduleButton extends StatefulWidget {
+  final int lectureId;
+
+  ScheduleButton({
+    @required this.lectureId
+  });
+
   @override
-  ScheduleButtonState createState() => new ScheduleButtonState();
+  ScheduleButtonState createState() => ScheduleButtonState(
+    lectureId: this.lectureId
+  );
 }
 
 class ScheduleButtonState extends State<ScheduleButton> {
-  int fabIconNumber = 0;
-  List<IconData> icons = [Icons.alarm_off_rounded, Icons.alarm_add_rounded];
-  IconData ic = Icons.alarm_add_rounded;
+  final int lectureId;
+  bool notificationActivated = false;
+
+  ScheduleButtonState({
+    @required this.lectureId
+  });
 
   void onPressed() {
     //add another function here if needed
     setState(() {
-      fabIconNumber = fabIconNumber % 2 == 0 ? 0 : 1;
-      ic = icons[fabIconNumber];
-      fabIconNumber++;
+      notificationActivated = !notificationActivated;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Align(
-        alignment: new Alignment(0.65, 0),
-        child: new Transform.scale(
+    return Align(
+        alignment: Alignment(0.65, 0),
+        child: Transform.scale(
             scale: 0.8,
-            child: new FloatingActionButton(
-                onPressed: onPressed, child: Icon(ic), heroTag: null)));
+            child: FloatingActionButton(
+                onPressed: onPressed,
+                child: Icon(
+                    notificationActivated ?
+                    Icons.alarm_off_rounded :
+                    Icons.alarm_add_rounded
+                ),
+                heroTag: null
+            )
+        )
+    );
   }
 }
